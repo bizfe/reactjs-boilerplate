f90622172b7aa5bdfb4b36a49a4c881551980e84
Yanming Guo <rockcoder@126.com | rockcoder23@gmail.com>
2016-01-27 17:58:27 +0800

    build with sigle  css file

--- model/defaultControl.js
+++ model/defaultControl.js
@@ -1,6 +1,6 @@
 exports.bind = function (php) {
 
     this.eventHandle.onOver = function (data) {
-        data.isDebug = config.jserver.isDebug;
+        data.isDebug = false && config.jserver.isDebug;
     }
 };
--- webpacks/webpack.config.prod.js
+++ webpacks/webpack.config.prod.js
@@ -9,8 +9,8 @@ module.exports = {
     lib: ['react', 'react-dom', 'react-redux', 'react-router', 'redux', 'redux-thunk']
   },
   output: {
-    path: path.join(__dirname, '../static/js/app/'),
-    filename: 'bundle.js',
+    path: path.join(__dirname, '../static/'),
+    filename: 'js/bundle.js',
     sourceMapFilename: '[file].map?' + new Date().getTime()
   },
   plugins: [
@@ -20,7 +20,8 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new webpack.optimize.CommonsChunkPlugin("lib", "lib.bundle.js"),
+    new webpack.optimize.CommonsChunkPlugin("lib", "js/lib.bundle.js"),
+    new ExtractTextPlugin("css/styles.css"),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
@@ -30,7 +31,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/,  loader: 'babel', include: path.join(__dirname, '../app'),  exclude: /node_modules/ },
-      { test: /\.less$/,  loader: 'style!css?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!less',  include: path.join(__dirname, '../app'),  exclude: /node_modules/},
+      { test: /\.less$/,  loader: ExtractTextPlugin.extract('style', 'css?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]','less', {publicPath: '../static/css/app/'}),  include: path.join(__dirname, '../app'),  exclude: /node_modules/},
+      // { test: /\.less$/,  loader: 'style!css?modules&importLoaders=1&localIdentName=[name]_[local]_[hash:base64:5]!less',  include: path.join(__dirname, '../app'),  exclude: /node_modules/},
       { test: /\.css$/,   loader: 'style!css?modules',  include: [path.join(__dirname, '../static/css/'), path.join(__dirname, '../app')],  exclude: /node_modules/},
       { test: /\.json$/,  loader: 'json-loader'}
     ]
