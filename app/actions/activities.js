a893dbbcec6d5deaecbe939896fb93ba19f48817
Yanming Guo <rockcoder@126.com | rockcoder23@gmail.com>
2016-01-28 12:12:38 +0800

    add eslint

--- app/actions/activities.js
+++ app/actions/activities.js
@@ -1,14 +1,15 @@
-import { 
-	UPDATE_ACTIVITY_BY_TAG, 
-	UPDATE_ACTIVITY_BY_TITLE,
-	UPDATE_ACTIVITY_BY_PAGER,
+import {
+    UPDATE_ACTIVITY_BY_TAG,
+    UPDATE_ACTIVITY_BY_TITLE,
+    UPDATE_ACTIVITY_BY_PAGER,
 
-	REQUEST_ACTIVITY_START,
-	REQUEST_ACTIVITY_FAIL,
-	REQUEST_ACTIVITY_SUCCESS, 
-	GET_TAGS, 
-	UPDATE_TAG_STATE
-} from '../constants/actionType';
+    REQUEST_ACTIVITY_START,
+    REQUEST_ACTIVITY_FAIL,
+    REQUEST_ACTIVITY_SUCCESS,
+    GET_TAGS,
+    UPDATE_TAG_STATE
+}
+from '../constants/actionType';
 
 // 使用fetch主要考虑到同构。
 import fetch from 'isomorphic-fetch';
@@ -20,11 +21,10 @@ import fetch from 'isomorphic-fetch';
  * @param  {[type]} title [description]
  * @return {[type]}       [description]
  */
-function _requestActivity (type, data) {
-	return {
-		type: type,
-		data: data
-	}
+function _requestActivity(type, data) {
+    return {
+        type, data
+    }
 }
 
 
@@ -33,30 +33,30 @@ function _requestActivity (type, data) {
  * @param  {[type]} tag [description]
  * @return {[type]}     [description]
  */
-export function getActivityByTag (tag) {
-	let url = `/aj/activity/getActivities?tagName=${tag}`;
-	let actionType = UPDATE_ACTIVITY_BY_TAG;
-	return dispatch => {
-		return fetch(url)
-				.then(res => {
-					if (res.ok){
-						return res.json()
-					} else {
+export function getActivityByTag(tag) {
+    let url = `/aj/activity/getActivities?tagName=${tag}`;
+    let actionType = UPDATE_ACTIVITY_BY_TAG;
+    return (dispatch) => {
+        return fetch(url)
+            .then((res) => {
+                if (res.ok) {
+                    return res.json()
+                } else {
 
-						// 请求失败
-						dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
-						return;
-					}
-				})
-				.then(data => {
-					if (data.code == 0) {
-						dispatch(_requestActivity(actionType, data));
-					} else {
-						dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
-					}
-				})
-				.catch(res => {
-					dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
-				})
-	}
+                    // 请求失败
+                    dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
+                    return;
+                }
+            })
+            .then((data) => {
+                if (data.code == 0) {
+                    dispatch(_requestActivity(actionType, data));
+                } else {
+                    dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
+                }
+            })
+            .catch((res) => {
+                dispatch(_requestActivity(REQUEST_ACTIVITY_FAIL, ''));
+            })
+    }
 }
